773bec466812fda27e169bfb52063e9b899d058c
Obaida Aljjarrah <Obaidaxt@gmail.com>
2023-01-25 13:47:49 +0100

    first commit

--- array3.dart
+++ array3.dart
@@ -1,34 +1,55 @@
-import 'dart:io';
-
-main() {
- 
-  int average = 0;
-
-  for (int i = 0; i < 3; i++) {
-    stdout.writeln('number');
-     String? input = stdin.readLineSync();
-    average+=(int.parse(input!));
-  }
- 
-  print('${average ~/ 3}');
-}
-
-
-
 // import 'dart:io';
 
 // main() {
- 
-//   List<int> score = [];
+
 //   int average = 0;
 
 //   for (int i = 0; i < 3; i++) {
 //     stdout.writeln('number');
-//      String? lenght = stdin.readLineSync();
-//     score.add(int.parse(lenght!));
+//      String? input = stdin.readLineSync();
+//      int convertToInt=int.parse(input!);
+//     average+=(convertToInt);
 //   }
-//   for (int i = 0; i < 3; i++) {
-//     average += score[i];
-//   }
-//   print('${average ~/ 3}');
+
+//   print('the average is ${average ~/ 3}');
 // }
+
+// import 'dart:io';
+
+import 'dart:io';
+
+main() {
+  // List<int> score = [1, 2, 2];
+  // int average = 0;
+
+  // for (int i = 0; i < 3; i++) {
+  //   stdout.writeln('number');
+  //   String? input = stdin.readLineSync();
+  //   int convert = int.parse(input!);
+  //   print('the convert is${score}');
+  //   score.add(convert);
+
+  //   average += score[i];
+  //   print('the convert is${score}');
+  // }
+  // for (int i = 0; i < 3; i++) {
+  //   average += score[i];
+  //   print(' ${score[i]}');
+  // }
+  // print(average~/3);
+  stdout.write('how many score? ');
+  String? input = stdin.readLineSync();
+  int length = int.parse(input!);
+  List<int> score = [1, 3, 5];
+  int result = 0;
+  for (int i = 0; i < length; i++) {
+    stdout.write('number: ');
+
+    String? input = stdin.readLineSync();
+    int convert = int.parse(input!);
+
+    score[i] = convert;
+    result += score[i];
+  }
+  print('the average is (${result ~/length})');
+}
